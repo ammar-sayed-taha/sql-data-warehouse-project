7038b94c7741b08676ab4894050bb62e3a862876
ammar-sayed-taha <ammarsayed669@gmail.com>
2025-11-29 12:31:08 +0000

    built bronze and silver stored procedures

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,22 +1,30 @@
+/* 
 --------------------------------------------------------------
--- Create Bronze Layer Tables one by one from the data sources
+-- DDL Script: Create Bronze Tables
 --------------------------------------------------------------
+Script Purpose: 
+  This script creates tables in the 'bronze' schema, dropping the existing tables
+  if they already exist.
+  Run this cript to re-define the DDL structure of 'bronze' Tables.
+--------------------------------------------------------------
+*/
+
 
 -- cust_info.csv
 CREATE OR REPLACE TABLE datawarehouse.bronze.crm_cust_info (
-  cst_id int,
+  cst_id string,
   cst_key string,
   cst_firstname string,
   cst_lastname string,
   cst_marital_status string,
-  cst_gndr char(1),
+  cst_gndr string,
   cst_create_date date
 );
 
 
 -- prd_info.csv
 CREATE OR REPLACE TABLE datawarehouse.bronze.crm_prd_info (
-  prd_id int,
+  prd_id string,
   prd_key string,
   prd_nm string,
   prd_cost int,
@@ -28,12 +36,12 @@ CREATE OR REPLACE TABLE datawarehouse.bronze.crm_prd_info (
 
 -- sales_details.csv
 CREATE OR REPLACE TABLE datawarehouse.bronze.crm_sales_details (
-  sls_ord_num int,
+  sls_ord_num string,
   sls_prd_key string,
   sls_cust_id int,
-  sls_order_dt date,
-  sls_ship_dt date,
-  sls_due_dt date,
+  sls_order_dt string,
+  sls_ship_dt string,
+  sls_due_dt string,
   sls_sales int,
   sls_quantity int,
   sls_price int
